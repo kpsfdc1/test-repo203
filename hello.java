5259b74ef111c1fc49285349a13241ee1b647c0a
dev1 <dev1@email.com>
2026-04-30 08:17:13 +0530

    by dev1

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
-class hello	#from dev2
-{
-	public static void main()	#from dev2
+class hello	#from dev2	..from dev1
+{				..from dev1
+	public static void main()	#from dev2	..from dev1
 	{
 		system.out.println("hello modf by dev1");
 	}
